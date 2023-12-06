10a11a286f4036b2dac2e7ec992a1f3613d03817
Eduardo Enrique <105998765+Duduenri@users.noreply.github.com>
2023-12-06 20:26:58 -0300

    adicionando estado

--- index.js
+++ index.js
@@ -3,28 +3,31 @@ const fs = require("fs");
 
 const times = [];
 const divisoes = [];
+const estados = [];
 
 function incluirTime() {
   console.log("\nInclusão de Times de Futebol");
   console.log("-----------------------------");
 
   const nome = prompt("Nome do Time: ");
+  const estado = prompt("Estado do Time: ");
   const divisao = Number(prompt("Divisão: "));
 
   times.push(nome);
+  estados.push(estado);
   divisoes.push(divisao);
 
-  console.log(`Tudo certo! '${nome}' cadastrado com sucesso na Divisão '${divisao}'`);
+  console.log(`Ok! Time '${nome}' cadastrado com sucesso na Divisão '${divisao}' no Estado '${estado}'`);
 }
 
 function listarTimes() {
   console.log("\nLista de Times de Futebol");
   console.log("-------------------------");
 
-  console.log("\nNome do Time............. Divisão..........");
+  console.log("\nNome do Time............. Estado........... Divisão..........");
 
   for (let i = 0; i < times.length; i++) {
-    console.log(`${times[i].padEnd(25)} ${divisoes[i].toString().padEnd(15)}`);
+    console.log(`${times[i].padEnd(25)} ${estados[i].padEnd(20)} ${divisoes[i].toString().padEnd(15)}`);
   }
 }
 
@@ -35,7 +38,19 @@ function pesquisarPorDivisao() {
 
   for (let i = 0; i < divisoes.length; i++) {
     if (divisoes[i] === divisaoPesquisa) {
-      console.log(`${times[i].padEnd(25)} ${divisoes[i].toString().padEnd(15)}`);
+      console.log(`${times[i].padEnd(25)} ${estados[i].padEnd(20)} ${divisoes[i].toString().padEnd(15)}`);
+    }
+  }
+}
+
+function pesquisarPorEstado() {
+  const estadoPesquisa = prompt("Digite o estado para pesquisa: ");
+  console.log("\nResultado da Pesquisa por Estado");
+  console.log("---------------------------------");
+
+  for (let i = 0; i < estados.length; i++) {
+    if (estados[i].toUpperCase() === estadoPesquisa.toUpperCase()) {
+      console.log(`${times[i].padEnd(25)} ${estados[i].padEnd(20)} ${divisoes[i].toString().padEnd(15)}`);
     }
   }
 }
@@ -48,18 +63,22 @@ function pesquisarPorIntervaloDivisao() {
 
   for (let i = 0; i < divisoes.length; i++) {
     if (divisoes[i] >= divisaoInicial && divisoes[i] <= divisaoFinal) {
-      console.log(`${times[i].padEnd(25)} ${divisoes[i].toString().padEnd(15)}`);
+      console.log(`${times[i].padEnd(25)} ${estados[i].padEnd(20)} ${divisoes[i].toString().padEnd(15)}`);
     }
   }
 }
 
 function alterarTime() {
-  const nomeTime = prompt("Digite o nome do time para alterar: ");
+  const nomeTime = prompt("Digite o nome do time para alteração: ");
   const index = times.indexOf(nomeTime);
 
   if (index !== -1) {
+    const novoEstado = prompt("Digite o novo estado do time: ");
     const novaDivisao = Number(prompt("Digite a nova divisão (número): "));
+
+    estados[index] = novoEstado;
     divisoes[index] = novaDivisao;
+
     console.log("Time alterado com sucesso!");
   } else {
     console.log("Time não encontrado.");
@@ -67,12 +86,14 @@ function alterarTime() {
 }
 
 function excluirTime() {
-  const nomeTime = prompt("Digite o nome do time para excluir: ");
+  const nomeTime = prompt("Digite o nome do time para exclusão: ");
   const index = times.indexOf(nomeTime);
 
   if (index !== -1) {
     times.splice(index, 1);
+    estados.splice(index, 1);
     divisoes.splice(index, 1);
+
     console.log("Time excluído com sucesso!");
   } else {
     console.log("Time não encontrado.");
@@ -83,7 +104,7 @@ function salvarDados() {
   const dados = [];
 
   for (let i = 0; i < times.length; i++) {
-    dados.push(`${times[i]};${divisoes[i]}`);
+    dados.push(`${times[i]};${estados[i]};${divisoes[i]}`);
   }
 
   fs.writeFileSync("dados/times.txt", dados.join("\n"));
@@ -97,7 +118,8 @@ function carregarDados() {
     for (let i = 0; i < dados.length; i++) {
       const partes = dados[i].split(";");
       times.push(partes[0]);
-      divisoes.push(Number(partes[1]));
+      estados.push(partes[1]);
+      divisoes.push(Number(partes[2]));
     }
   }
 }
@@ -112,12 +134,13 @@ do {
   console.log("1. Incluir Time");
   console.log("2. Listar Times");
   console.log("3. Pesquisar por Divisão");
-  console.log("4. Pesquisar por Intervalo de Divisão");
-  console.log("5. Alterar Time");
-  console.log("6. Excluir Time");
-  console.log("7. Finalizar");
+  console.log("4. Pesquisar por Estado");
+  console.log("5. Pesquisar por Intervalo de Divisão");
+  console.log("6. Alterar Time");
+  console.log("7. Excluir Time");
+  console.log("8. Finalizar");
 
-  opcao = Number(prompt("Opção: ")); 
+  opcao = Number(prompt("Opção: "));  
 
   if (opcao === 1) {
     incluirTime();
@@ -126,16 +149,18 @@ do {
   } else if (opcao === 3) {
     pesquisarPorDivisao();
   } else if (opcao === 4) {
-    pesquisarPorIntervaloDivisao();
+    pesquisarPorEstado();
   } else if (opcao === 5) {
-    alterarTime();
+    pesquisarPorIntervaloDivisao();
   } else if (opcao === 6) {
-    excluirTime();
+    alterarTime();
   } else if (opcao === 7) {
+    excluirTime();
+  } else if (opcao === 8) {
     console.log("Programa finalizado.");
   } else {
     console.log("Opção inválida. Tente novamente.");
   }
-} while (opcao !== 7);
+} while (opcao !== 8);
 
 salvarDados();
